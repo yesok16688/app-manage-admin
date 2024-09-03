fd9fe02d7e304f093b40fc0bfef902f155407ab9
ok <ok@gmail.com>
2024-09-03 12:59:34 +0800

    save

--- config/app.php
+++ config/app.php
@@ -70,8 +70,8 @@ return [
     |
     */
 
-    // 'timezone' => 'UTC',
-    'timezone' => 'Asia/Shanghai',
+    'timezone' => 'UTC',
+    //'timezone' => 'Asia/Shanghai',
 
     /*
     |--------------------------------------------------------------------------
